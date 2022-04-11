5e87432c68caad1bddf3c5496e6e346a2bad5962
Wasil <death888080@gmail.com>
2022-04-11 23:54:24 +0200

    homework

---
+++ ex_1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int MakeIntFromStr(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int Stop = MakeIntFromStr("Сколько значений Вы хотите посчитать?: ");
+int count = 0;
+
+while (Stop != 0)
+{
+    int input = MakeIntFromStr("Введите число: ");
+    if (input > 0) count++;
+    Stop--;
+}
+
+Console.WriteLine($"Вы ввели чисел больше 0:- {count}.");
---
+++ ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿double MakeDobFromStr(string message)
+{
+    Console.Write(message);
+    double num = double.Parse(Console.ReadLine());
+    return num;
+}
+
+double b1 = MakeDobFromStr("Введите значение b1: ");
+double b2 = MakeDobFromStr("Введите значение b2: ");
+double k1 = MakeDobFromStr("Введите значение k1: ");
+double k2 = MakeDobFromStr("Введите значение k2: ");
+
+double X = (b2 - b1) / (k2 - k1);
+double Y = X * k2 + b2;
+
+Console.WriteLine($"Координаты двух прямых{(X, Y)}");
